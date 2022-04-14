d837fc13da0aa3faf47ae24a2d22fd3308856841
AFRA <81957946+afra-af@users.noreply.github.com>
2022-04-15 03:12:22 +0530

    Create pattern.cpp

---
+++ pattern.cpp
@@ -0,0 +1,21 @@
+  /*  *****
+    *****
+    *****
+    *****
+    *****
+    */
+ #include<iostream>
+ using namespace std;
+ int main(){
+    int n;
+  cin>>n;
+  int r,c;
+  for(r=1;r<=n;r++){
+  	for(c=1;c<=n;c++){ // number of stars= n in every row and col
+  		cout<<"*";
+	  }
+	  cout<<endl;
+
+	  }
+	   
+   }
