af937d6c59e39fb260fe1c9ed80d516b74d04f38
AFRA <81957946+afra-af@users.noreply.github.com>
2022-04-15 03:14:21 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,21 @@
+/*  *
+    **
+    ***
+    ****
+    *****
+    */
+#include<iostream>
+using namespace std;
+int main(){
+ int n;
+  cin>>n;
+  int r,c;
+  for(r=1;r<=n;r++){
+  	for(c=1;c<=r;c++){ // as r==c when r=1 c=1 then next r=2 print 2 *'s
+  		cout<<"*";
+	  }
+	  cout<<endl;
+
+	  }
+	   
+   }
