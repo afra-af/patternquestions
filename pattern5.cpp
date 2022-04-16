4ee1c8dfe3b21a08ef33d12e58a623e245599b2f
AFRA <81957946+afra-af@users.noreply.github.com>
2022-04-16 17:46:11 +0530

    Create pattern5.cpp

---
+++ pattern5.cpp
@@ -0,0 +1,18 @@
+/*    *****
+    ****
+    ***
+    **
+    *
+ */
+ int n;
+  cin>>n;
+  int i=0;
+  while(i<=n){
+  	int j=1;
+  	while(j<=n-i){
+  		cout<<"*";
+  	    j=j+1;
+    } 
+	  cout<<endl;
+	  i=i+1;
+  }
