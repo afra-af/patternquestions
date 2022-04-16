e9bc5868ad8f97bcada8eea510494e7f74d1eb95
AFRA <81957946+afra-af@users.noreply.github.com>
2022-04-16 18:46:36 +0530

    Create pattern8.cpp

---
+++ pattern8.cpp
@@ -0,0 +1,20 @@
+/*
+AAAAA
+BBBBB
+CCCCC
+DDDDD
+EEEEE
+*/
+int n;
+  cin>>n;
+  int i=1;
+  while(i<=n){
+  	int j=1;
+  	while(j<=n){
+  		char ch='A'+i-1;
+  		cout<<ch;
+  	    j=j+1;
+	  }
+	  cout<<endl;
+	  i=i+1;
+  }
