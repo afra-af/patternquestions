49cd66aa409e6daef898877de0dffe470602a64b
AFRA <81957946+afra-af@users.noreply.github.com>
2022-04-16 17:18:12 +0530

    Create pattern3.cpp

---
+++ pattern3.cpp
@@ -0,0 +1,21 @@
+/*1
+22
+333
+4444
+55555
+*/
+#include<stdio.h>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  int i=1;
+  while(i<=n){
+    int j=0;
+    while(j<=i){
+    cout<<i;
+      j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+  }}
